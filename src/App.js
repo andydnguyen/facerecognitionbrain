c575a6edf85270dd4d28cbaee7034d2696017923
Andy Nguyen <anguyen1119@gmail.com>
2019-12-03 22:58:46 -0500

    Add ImageLinkform components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
+
+const app = new Clarifai.App({
+  apiKey: process.env.REACT_APP_KEY
+});
 
 const particlesOptions = {
   particles: {
@@ -18,6 +23,33 @@ const particlesOptions = {
   }
 };
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: ''
+    };
+  }
+
+  onInputChange = e => {
+    console.log(e.target.value);
+  };
+
+  onButtonSubmit = () => {
+    console.log('click');
+    app.models
+      .predict(
+        'a403429f2ddf4b49b307e318f00e528b',
+        'https://samples.clarifai.com/face-det.jpg'
+      )
+      .then(
+        function(response) {
+          // do something with response
+        },
+        function(err) {
+          // there was an error
+        }
+      );
+  };
   render() {
     return (
       <div className='App'>
@@ -25,7 +57,10 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
         {/*<FaceRecognition />*/}
       </div>
     );

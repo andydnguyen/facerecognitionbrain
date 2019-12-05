3b8c611dbf3f331779bf96a4101205a46dd9f227
Andy Nguyen <anguyen1119@gmail.com>
2019-12-04 19:47:21 -0500

    Add FaceRecognition component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Rank from './components/Rank/Rank';
 import './App.css';
 
@@ -26,29 +27,28 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      input: ''
+      input: '',
+      imageUrl: ''
     };
   }
 
   onInputChange = e => {
-    console.log(e.target.value);
+    this.setState({ input: e.target.value });
   };
 
   onButtonSubmit = () => {
-    console.log('click');
-    app.models
-      .predict(
-        'a403429f2ddf4b49b307e318f00e528b',
-        'https://samples.clarifai.com/face-det.jpg'
-      )
-      .then(
-        function(response) {
-          // do something with response
-        },
-        function(err) {
-          // there was an error
-        }
-      );
+    this.setState({ imageUrl: this.state.input });
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
+      function(response) {
+        // do something with response
+        console.log(
+          response.outputs[0].data.regions[0].region_info.bounding_box
+        );
+      },
+      function(err) {
+        // there was an error
+      }
+    );
   };
   render() {
     return (
@@ -61,7 +61,7 @@ class App extends Component {
           onInputChange={this.onInputChange}
           onButtonSubmit={this.onButtonSubmit}
         />
-        {/*<FaceRecognition />*/}
+        <FaceRecognition imageUrl={this.state.imageUrl} />
       </div>
     );
   }
